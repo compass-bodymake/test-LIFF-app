979d7d8fd72d74d57915ca2acc7577e2a860582f
kaito-miyamoto-macho <t.ali.j.kk@gmail.com>
2025-07-02 15:22:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function getGASUrl() {
         // 本番環境
         ? "https://script.google.com/macros/s/AKfycbw_qZ108jgUiDIIzmaPW6vCB9oVI24qRYpyE36qNVsRdHCpwXzP9Dbz0DmdpGBwR9Mk/exec"
         // テスト環境
-        : "https://script.google.com/macros/s/AKfycbz2dI1rSkaMMkRwdu2rmIMh0dIBY1SkLWcXGcqZ8zOZfsCFCIVLMkeyEogF_V3pkl2NcA/exec";
+        : "https://script.google.com/macros/s/AKfycbwN_5b1GMg0qdZdJEzIUMLokleMaUCH3nst8ZJIwfl_UJDq_WcDEfMHK-pBM1TWc9Fdmw/exec";
 }
 
 // ✅ URLパラメータを取得する関数
